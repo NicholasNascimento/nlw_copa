d646dc6f92fa3c02c2144c2c8112d050ff6cc970
Nicholas <nicholas.an99@gmail.com>
2022-11-03 02:32:39 -0300

    Updating second class content

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,22 @@
+// interface HomeProps {
+//   count: number;
+// }
+
+export default function Home() {
+  return (
+    <div></div>
+  )
+}
+
+// export const getServerSideProps = async () => {
+//   const response = await fetch('http://localhost:3333/pools/count')
+//   const data = await response.json()
+  
+//   console.log(data)
+
+//   return {
+//     props: {
+//       count: data.count,
+//     }
+//   }
+// }
